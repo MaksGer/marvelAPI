d425a257996a0caa112d8b00f211a510043e980f
mherasimovich <mherasimovich@exadel.com>
2020-05-05 11:19:47 +0300

    New branch for prod

--- src/app/layouts/main-page-layout/main-page-layout.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-	selector: 'app-main-page-layout',
-	templateUrl: './main-page-layout.component.html',
-})
-export class MainPageLayoutComponent { }
